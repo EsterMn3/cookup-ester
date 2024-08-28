42da77b951d082b75a9539679317da79f30fdea3
EsterMn3 <esterminja86@gmail.com>
2024-08-29 00:40:27 +0200

    Added declare.d.ts file

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,6 +1,6 @@
 import View from './View.js';
 import icons from 'url:../../img/icons.svg';
-import { Fraction } from 'fraction.js';
+import { Fraction } from 'fractional';
 
 class RecipeView extends View {
   //parent element will be unique for each view
